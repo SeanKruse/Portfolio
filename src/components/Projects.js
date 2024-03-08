15d8bbfec62cf37299b1eee74aa7955b88f12150
Sean Kruse <seanfkruse@gmail.com>
2024-03-07 23:24:20 -0700

    Fix Gif Height

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,7 @@ export default function Projects() {
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative" style={{ height: "600px" }}>
+              <div className="flex relative" style={{ height: "300px" }}>
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
