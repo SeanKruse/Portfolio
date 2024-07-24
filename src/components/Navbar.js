f349f34a2174a5745c97a70a835edbfbc0cee4d1
Sean Kruse <seanfkruse@gmail.com>
2024-07-23 22:37:56 -0600

    Updated Hrefs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,15 +14,15 @@ export default function Navbar() {
           <a href="#/projects" className="mr-5 hover:text-white">
             Past Work
           </a>
-          <a href="/#skills" className="mr-5 hover:text-white">
+          <a href="#/skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="/#testimonials" className="mr-5 hover:text-white">
+          <a href="#/testimonials" className="mr-5 hover:text-white">
             Testimonials
           </a>
         </nav>
         <a
-          href="/#contact"
+          href="#/contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
